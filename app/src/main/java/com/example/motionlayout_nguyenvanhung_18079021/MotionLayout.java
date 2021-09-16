c4b038ebfb297b9471d89f5e106f72588fa5bb80
hung2504 <vanhung25042000@gmail.com>
2021-09-16 11:07:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/motionlayout_nguyenvanhung_18079021/MotionLayout.java
@@ -0,0 +1,14 @@
+package com.example.motionlayout_nguyenvanhung_18079021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_layout);
+    }
+}
