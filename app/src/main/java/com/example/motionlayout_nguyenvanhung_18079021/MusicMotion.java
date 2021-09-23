9600ebdfc859579366b9e7e92337ad2318abeec8
hung2504 <vanhung25042000@gmail.com>
2021-09-23 15:53:42 +0700

    Music motion

---
+++ app/src/main/java/com/example/motionlayout_nguyenvanhung_18079021/MusicMotion.java
@@ -0,0 +1,14 @@
+package com.example.motionlayout_nguyenvanhung_18079021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicMotion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_motion);
+    }
+}
